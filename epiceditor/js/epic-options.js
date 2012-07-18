113614b05c4b78e33af747b35d5dd7c6c3eb0e6f
Peter Anderson <diniremix@gmail.com>
2012-07-18 16:36:20 -0500

    agregado archivo de opciones para Epic Editors

---
+++ epiceditor/js/epic-options.js
@@ -0,0 +1,27 @@
+$(function() {
+var opts = {
+  container: 'epiceditor',
+  basePath: 'epiceditor',
+  clientSideStorage: true,
+  localStorageName: 'epiceditor',
+  parser: marked,
+  file: {
+    name: 'epiceditor',
+    defaultContent: '',
+    autoSave: 100
+  },
+  theme: {
+    base:'/themes/base/epiceditor.css',
+    preview:'/themes/preview/github.css',
+    editor:'/themes/editor/epic-dark.css'
+  },
+  focusOnLoad: false,
+  shortcut: {
+    modifier: 18,
+    fullscreen: 70,
+    preview: 80,
+    edit: 79
+  }
+}
+  var editor = new EpicEditor(opts).load();
+});
